1cda4fa44874eba01bb8beffdabfdd37a1f2ab49
Ray Terrill <rayjohnterrill@gmail.com>
2018-01-22 04:24:18 +0000

    Added the ability to get image sizes dynamically and set the canvas size based on uploaded image.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+var sizeOf = require('image-size');
+
 
 //setup aws
 var AWS = require('aws-sdk');
@@ -25,7 +27,10 @@ app.get('/', function (req, res) {
 app.post('/upload', type, function (req,res) {
   console.log(req.body); //form fields
   console.log(req.file); //form files
-  
+
+  var dimensions = sizeOf(req.file.buffer);
+  console.log(dimensions.width, dimensions.height);
+
   var base64image = new Buffer(req.file.buffer).toString('base64');
   var base64imageForAWS = new Buffer(req.file.buffer, 'base64');
   
@@ -41,15 +46,18 @@ app.post('/upload', type, function (req,res) {
     if (err) {
       console.log(err, err.stack); // an error occurred
     } else {
-      //console.log(data);           // successful response
-      console.log(data.FaceDetails[0]);
+      console.log(data.FaceDetails);
+//      console.log(data.FaceDetails[0]);
+
+      var leftEye = data.FaceDetails[0].Landmarks.find(o => o.Type === 'eyeLeft');
+      var rightEye = data.FaceDetails[0].Landmarks.find(o => o.Type === 'eyeRight');
+
       var faceDetails = data.FaceDetails[0];
     }
   
   //res.send('<img src="data:image/gif;base64,' + base64image + '" style="max-height: 400px; max-width: 400px;" />');
   var imageSrc = 'data:image/gif;base64,' + base64image;
-  res.render('upload', { title: 'Hey', uploadedImage: imageSrc, low: faceDetails.AgeRange.Low, high: faceDetails.AgeRange.High, smile: faceDetails.Smile, emotion: faceDetails.Emotions[0], beard: faceDetails.Beard});
-  
+  res.render('upload', { title: 'Hey', uploadedImage: imageSrc, low: faceDetails.AgeRange.Low, high: faceDetails.AgeRange.High, smile: faceDetails.Smile, emotion: faceDetails.Emotions[0], beard: faceDetails.Beard, leftEye: leftEye, rightEye: rightEye, imageWidth: dimensions.width, imageHeight: dimensions.height});
   });
   
   //res.status(204).end();
